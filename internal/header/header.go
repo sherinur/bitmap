dc7b7ec0f158d3aebf23b0161585441117d06e15
nsheri <sherinurislam@gmail.com>
2024-09-01 18:34:45 +0500

    bmp parser

--- internal/header/header.go
+++ internal/header/header.go
@@ -2,6 +2,7 @@ package header
 
 import (
 	"fmt"
+<<<<<<< HEAD
 
 	"bitmap/internal/bmp"
 )
@@ -13,6 +14,26 @@ func Execute(filepath string) error {
 	}
 
 	fmt.Println(header)
+=======
+	"os"
+)
+
+func Execute(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// ? CONTINUE HERE ...
+	// img, err := bmp.DecodeBMP(file)
+	// if err != nil {
+	// 	return err
+	// }
+
+	fmt.Println("Image decoded successfully.")
+	// fmt.Println(img)
+>>>>>>> dd64ac9fe426181de0efb39504f79a2887cf2365
 
 	return nil
 }
